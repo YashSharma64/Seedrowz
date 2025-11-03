204ce1a3240c6b80fb6244eb302ac3dfed65dc02
Yash Sharma <yashsharma.aiml@gmail.com>
2025-11-03 18:29:06 +0530

    Sync backend and frontend structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-900">
+      <h1 className="text-5xl font-bold text-blue-400">Tailwind is Working! ⚡</h1>
+    </div>
+  );
+}
